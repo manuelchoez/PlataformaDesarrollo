db1f9c0f71fc9820852ab3e98e5efe23bab2f676
manuelchoez <40345380+manuelchoez@users.noreply.github.com>
2022-05-25 22:29:21 -0500

    implementacion de servicios

---
+++ SQL/Codigo/PlataformaDesarrollo/PlataformaDesarrollo.Servicio/Request.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlataformaDesarrollo.Servicio
+{
+    public class Request<TRequest>
+    {
+        public int Transaccion { get; set; }
+        public string IdSession { get; set; }
+        public string Secuencial { get; set; }
+        public TRequest Mensaje { get; set; }
+    }
+}
---
+++ SQL/Codigo/PlataformaDesarrollo/PlataformaDesarrollo.Servicio/Response.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlataformaDesarrollo.Servicio
+{
+    public class Response<TResponse>
+    {
+        public HttpStatusCode Status { get; set; }
+        public bool EsError { get; set; }
+        public TResponse Result { get; set; }
+        public object Mensaje { get; set; }
+        public Response(TResponse response, object mensaje)
+        {
+            Result = response;
+            EsError = false;
+            Mensaje = mensaje;
+            Status = HttpStatusCode.OK;
+        }
+
+        protected Response(Exception errors)
+        {
+            Mensaje = errors.Message;
+            EsError = true;
+            Status = HttpStatusCode.InternalServerError;
+        }
+
+        protected Response(object mensajeControlado)
+        {
+            Mensaje = mensajeControlado;
+            EsError = false;
+            Status = HttpStatusCode.OK;     
+        }
+
+        public static Response<TResponse> Error(Exception error)
+        {
+            return new Response<TResponse>(error);
+        }
+
+        public static Response<TResponse> OK(TResponse response, object mensaje)
+        {
+            return new Response<TResponse>(response, response);
+        }
+        public static Response<TResponse> Warnig(object mensajeControlado)
+        {
+            return new Response<TResponse>(mensajeControlado);
+        }
+    }
+}
